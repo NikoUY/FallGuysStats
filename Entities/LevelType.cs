034817727e9f3a414648423000e00a2df37a5dd2
Niko <nikod.uy@gmail.com>
2020-10-08 15:14:34 -0300

    Merge switch statements

--- Entities/LevelType.cs
+++ Entities/LevelType.cs
@@ -9,14 +9,12 @@
     static class LevelTypeBehavior {
         public static int FastestLabel(this LevelType type) {
             switch (type) {
-                case LevelType.Race:
+                case LevelType.Race: case LevelType.Hunt:
                     return 1; // FASTEST
                 case LevelType.Survival:
                     return 0; // LONGEST
                 case LevelType.Team:
                     return 2; // HIGH_SCORE
-                case LevelType.Hunt:
-                    return 1; // FASTEST
             }
             return 1;
         }
